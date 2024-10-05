17d033f16c5909f8248ea74781da17f685863214
TuanAnhNguyen-2005 <Tn271574@gmail.com>
2024-10-05 22:00:01 +0700

    init project

---
+++ Cau_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Cau_1 {
+    public static void main(String[] args) {
+        String ten = "Nguyen Anh Tuan";
+        int tuoi = 30;
+        double chieucao = 1.72;
+        boolean laptrinh = true;
+        System.out.println("ten: "+ ten);
+        System.out.println("tuoi: "+ tuoi);
+        System.out.println("chieu cao: "+ chieucao);
+        System.out.println("co thich lap trinh khong (true/false): "+ laptrinh);
+    }
+}
